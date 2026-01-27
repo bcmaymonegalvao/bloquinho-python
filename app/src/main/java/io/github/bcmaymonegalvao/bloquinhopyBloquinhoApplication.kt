02ed308ae08baec58afb3946ec4ba48d27391d8e
Bruno César Maymone Galvão <139981685+bcmaymonegalvao@users.noreply.github.com>
2026-01-26 23:56:05 -0300

    Add BloquinhoApplication class with Hilt supportfeat: add BloquinhoApplication with Hilt setup

---
+++ app/src/main/java/io/github/bcmaymonegalvao/bloquinhopyBloquinhoApplication.kt
@@ -0,0 +1,7 @@
+BloquinhoApplication.ktpackage io.github.bcmaymonegalvao.bloquinhopy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BloquinhoApplication : Application()
